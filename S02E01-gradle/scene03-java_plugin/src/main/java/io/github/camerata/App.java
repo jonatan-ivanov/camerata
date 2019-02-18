8ce5020188c6de9a1c9ee720aa92e20107db7098
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2019-02-17 17:16:37 -0800

    lombok + logging for scene03-java_plugin

--- S02E01-gradle/scene03-java_plugin/src/main/java/io/github/camerata/App.java
+++ S02E01-gradle/scene03-java_plugin/src/main/java/io/github/camerata/App.java
@@ -1,8 +1,11 @@
 package io.github.camerata;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class App {
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        log.info(new App().getGreeting());
     }
 
     String getGreeting() {
